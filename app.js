70d2fd8bbbc80fe14c74076fefa8cf786b6d11f5
Silvia Gonzalez <devsoftwaregonzalez@gmail.com>
2024-07-08 15:55:43 -0300

    se añaden modificaciones a app.js y Readme.md con desafios finales

--- app.js
+++ app.js
@@ -1,16 +1,41 @@
 //Variables
-let numeroSecreto = 6;
-let numeroUsuario = prompt("Me indicas un número entre 1 y 10 por favor:");
+let numeroMaximoPosible = 10000;
+let numeroSecreto = Math.floor(Math.random() * numeroMaximoPosible) + 1;
+let numeroUsuario = 0;
+let intentos = 1;
+//let palabraVeces = 'vez';
+let maximosIntentos = 20;
 
-console.log(numeroUsuario);
-/*
-Este código realiza
-la comparación
-*/
-if (numeroUsuario == numeroSecreto) {
+while (numeroUsuario != numeroSecreto) {
+  numeroUsuario = parseInt(
+    prompt(`Me indicas un número entre 1 y ${numeroMaximoPosible}  por favor:`)
+  );
+
+  console.log(typeof numeroUsuario);
+  if (numeroUsuario == numeroSecreto) {
     //Acertamos, fue verdadera la condición
-    alert(`Acertaste, el número es: ${numeroUsuario}`);
-} else {
+    alert(
+      `Acertaste, el número es: ${numeroUsuario}. Lo hiciste en ${intentos} ${
+        intentos == 1 ? "vez" : "veces"
+      }`
+    );
+  } else {
+    if (numeroUsuario > numeroSecreto) {
+      alert("El número secreto es menor");
+    } else {
+      alert("El número secreto es mayor");
+    }
+    //Incrementamos el contador cuando no acierta
+    //intentos = intentos + 1;
+    //intentos += 1;
+    intentos++;
+
+    //palabraVeces = 'veces';
+    if (intentos > maximosIntentos) {
+      alert(`Llegaste al número máximo de ${maximosIntentos} intentos`);
+      break;
+    }
     //La condición no se cumplió
-    alert('Lo siento, no acertaste el número');
+    //alert('Lo siento, no acertaste el número');
+  }
 }
