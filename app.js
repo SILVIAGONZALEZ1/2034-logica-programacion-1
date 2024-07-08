df530d3fd1cf4a311eef1a49d0e54452393d500c
Silvia Gonzalez <devsoftwaregonzalez@gmail.com>
2024-07-08 00:59:37 -0300

    Se agregan loops y tentativas

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
 //Variables
-let numeroSecreto = 6;
-let numeroUsuario = prompt("Me indicas un número entre 1 y 10 por favor:");
 
-console.log(numeroUsuario);
-/*
-Este código realiza
-la comparación
-*/
-if (numeroUsuario == numeroSecreto) {
+let numeroSecreto = 4;
+let numeroUsuario = 0;
+let intentos = 1;
+let palabraVeces = 'vez'
+while (numeroUsuario != numeroSecreto) {
+  numeroUsuario = prompt("Me indicas un número entre 1 y 10 por favor:");
+
+  console.log(numeroUsuario);
+  //Este código realiza la comparación
+  if (numeroUsuario == numeroSecreto) {
     //Acertamos, fue verdadera la condición
-    alert(`Acertaste, el número es: ${numeroUsuario}`);
-} else {
-    //La condición no se cumplió
-    alert('Lo siento, no acertaste el número');
+    alert(`Acertaste, el numero es: ${numeroUsuario}. Lo hiciste en ${intentos} ${palabraVeces}`);
+  } else {
+    if (numeroUsuario > numeroSecreto) {
+      alert("El numero secreto es menor");
+    } else {
+      //La condición no se cumplió
+      alert("El numero secreto es mayor");
+    }
+    intentos = intentos + 1;
+    palabraVeces = 'veces';
+  }
 }
